0972a78463154ac4df00db286e5f56a4cc2578d7
ttt <t0127mikaken@gmail.com>
2024-04-04 06:43:17 +0900

    practice git

--- trial.cpp
+++ trial.cpp
@@ -6,8 +6,13 @@
 int main(){
     std::vector<std::vector<double>> a = {{1,2,3}, {1,2,3}};
     std::vector<std::vector<double>> b = {{3,3,3}, {2,2,7},{4,5,6} };
+    std::vector<std::vector<double>> c = {{2,2,7},{4,5,6}};
 
-    print(a * b);
+
+    auto ans = c- a * (dot(a,b)  + a);
+
+
+    print(ans);
 
 
 
