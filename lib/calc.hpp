c6a2e37234b4e4ef6275ff2794deff15adb0c85b
ttt <t0127mikaken@gmail.com>
2024-05-13 21:10:55 +0900

    update lib

---
+++ lib/calc.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "add.hpp"
+#include "sub.hpp"
+#include "mul.hpp"
+#include "div.hpp"
+#include "exp.hpp"
+#include "log.hpp"
+#include "cos.hpp"
+#include "sin.hpp"
+
+
+template<typename T>
+auto tanh(double x){
+
+    return  (exp(x) - exp(-1 * x)) / (exp(x) + exp(-1 * x));
+}
+
+template<typename T>
+auto tanh(std::vector<T>&& x){
+
+    return  (exp(x) - exp(-1 * x)) / (exp(x) + exp(-1 * x));
+}
+
+// 左辺値のインターフェース
+template<typename T>
+auto tanh(std::vector<T>& x){
+    return  tanh(std::move(x));
+}
+
+
+template<typename T>
+auto sigmoid(double x){
+  
+    return 1.0/(1.0 + exp(-1 * x));
+
+}
+
+template<typename T>
+auto sigmoid(std::vector<T>&& x){
+  
+    return 1.0/(1.0 + exp(-1 * x));
+
+}
+
+// 左辺値のインターフェース
+template<typename T>
+auto sigmoid(std::vector<T>& x){
+  
+    return sigmoid(std::move(x));
+}
+
+
